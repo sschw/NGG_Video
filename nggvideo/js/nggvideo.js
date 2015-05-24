9ef5d78631cc771d9944d81cc12c722bf2daf499
Sandro Schwager <sandro.schwager@bsiag.com>
2015-05-24 17:48:40 +0200

    Some small changes for making script presentable

--- nggvideo/js/nggvideo.js
+++ nggvideo/js/nggvideo.js
@@ -1,21 +1,50 @@
+var owner = null;
+
 jQuery(function($){
-  /*$(window).load(function() {
-    $('.nggvideo-thumbnail a').each(function() {
-      if($(this).data("video") != "") {
-        $(this).click(function() {
-          var owner = this;
-          // workaround - can't stopp ngg from loading image or run script immediately afterwards
-          window.setTimeout(function() {
-            var createdObj = $('img[src$="' + $(owner).data("src")+ '"]');
-            var video = $(owner).data("video");
-            createdObj.replaceWith("<div class=\"nggvideo\" style=\"display: inline-block; margin: auto;\">" + video + "<div>");
-            /*var videoWidth = parseInt(video.match("width[:=].? *([0-9]+)[^0-9]")[1]);
-            var videoHeight = parseInt(video.match("height[:=].? *([0-9]+)[^0-9]")[1]);
-            createdObjCont.width(videoWidth);
-            createdObjCont.height(videoHeight);*/
-    /*      }, 100);
-        });
+  var loadVideo = function() {
+    // workaround - can't stopp ngg from loading image or run script immediately afterwards
+    waitForImg = function() {
+      if($('img[src$="' + owner.data("src")+ '"]').length == 1) {
+        var createdObj = $('img[src$="' + owner.data("src")+ '"]');
+        var video = owner.data("video");
+        var videoWidth = parseInt(video.match("width[:=].? *([0-9]+)[^0-9]")[1]);
+        var videoHeight = parseInt(video.match("height[:=].? *([0-9]+)[^0-9]")[1]);
+        createdObj.replaceWith("<div class=\"nggvideo\" style=\"width: " + videoWidth + "px; height: " + videoHeight + "px; margin: auto;\">" + video + "<div>");
+        /*createdObjCont.width(videoWidth);
+        createdObjCont.height(videoHeight);*/
+      } else {
+        window.setTimeout(waitForImg, 10);
+      }
+    }
+    waitForImg();
+  }
+  
+  $(window).load(function() {
+    // handler on open gallery
+    $('.nggvideo-thumbnail a').click(function() {
+      owner = $(this);
+      if(owner.data("video") != "") {
+        loadVideo();
       }
     });
-  });*/
+    
+    // workaround - can't trigger on next or prev by using different libraries - so just add triggers for all gallery libraries
+    // handler for different galleries
+    $('#fancybox-right').click(function() {
+      if(owner != null) {
+        owner = $(owner.closest(".ngg-gallery-thumbnail-box").next().find(".nggvideo-thumbnail a")[0]);
+        if(owner.data("video") != "") {
+          loadVideo();
+        }
+      }
+    });
+    $('#fancybox-left').click(function() {
+      if(owner != null) {
+        owner = $(owner.closest(".ngg-gallery-thumbnail-box").prev().find(".nggvideo-thumbnail a")[0]);
+        if(owner.data("video") != "") {
+          loadVideo();
+        }
+      }
+    });
+  });
 });
